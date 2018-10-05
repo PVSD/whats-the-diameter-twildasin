92ddfd37ab15c527a89de9a3d9c3b0aa98b89eee
twildasin <tjwild127@aol.com>
2018-10-05 06:37:18 -0400

    final commit

---
+++ src/com/company/Circle.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Circle {
+
+    public double radius;
+
+    public Circle (double d){
+        radius = d;
+    }
+    public double diamter()
+    {
+        double d = radius * 2;
+        return d;
+    }
+
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Circle cir1 = new Circle(35.5);
+        System.out.println(cir1.diamter());
     }
 }
